29ab35108f6dbc89f1d2c2a6e24d8243521f0f58
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-05-02 12:00:22 -0500

    Actualizacion de cometarios de los métodos de PriorityQueue en actividad 3

--- actividad3/PriorityQueue.java
+++ actividad3/PriorityQueue.java
@@ -5,7 +5,7 @@ import actividad1.ExceptionIsEmpty;
 public interface PriorityQueue<E, N> {
     void enqueue(E x, N pr); // Inserta un elemento según su prioridad
     E dequeue() throws ExceptionIsEmpty; // Elimina y retorna el elemento con mayor prioridad
-    E front() throws ExceptionIsEmpty; // Solo retorna el elemento con mayor prioridad
-    E back() throws ExceptionIsEmpty; // Solo retorna el elemento con menor prioridad
+    E front() throws ExceptionIsEmpty; // Retorna el dato del elemento con mayor prioridad
+    E back() throws ExceptionIsEmpty; // Retorna el dato del elemento con menor prioridad
     boolean isEmpty(); // Retorna true si la cola está vacía, false en caso contrario
 }

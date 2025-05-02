e37e9b6b31124eb7215a30dfe13377ceca17e0d4
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-05-02 11:55:26 -0500

    Se añadió comentarios de los métodos de PriorityQueue en actividad 3

--- actividad3/PriorityQueue.java
+++ actividad3/PriorityQueue.java
@@ -3,9 +3,9 @@ package actividad3;
 import actividad1.ExceptionIsEmpty;
 
 public interface PriorityQueue<E, N> {
-    void enqueue(E x, N pr);
-    E dequeue() throws ExceptionIsEmpty;
-    E front() throws ExceptionIsEmpty;
-    E back() throws ExceptionIsEmpty;
-    boolean isEmpty();
+    void enqueue(E x, N pr); // Inserta un elemento según su prioridad
+    E dequeue() throws ExceptionIsEmpty; // Elimina y retorna el elemento con mayor prioridad
+    E front() throws ExceptionIsEmpty; // Solo retorna el elemento con mayor prioridad
+    E back() throws ExceptionIsEmpty; // Solo retorna el elemento con menor prioridad
+    boolean isEmpty(); // Retorna true si la cola está vacía, false en caso contrario
 }

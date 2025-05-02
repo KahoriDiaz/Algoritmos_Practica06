5adb3a0649e5adc34a9d660d6ba65eae9273aefd
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-05-02 14:09:41 -0500

    Desarrollo del metodo enqueue de la clase PriorityQueueLinkSort en actividad 3

--- actividad3/PriorityQueueLinkSort.java
+++ actividad3/PriorityQueueLinkSort.java
@@ -17,13 +17,44 @@ public class PriorityQueueLinkSort<E, N extends Comparable<N>> implements Priori
     private Node<EntryNode> first;
     private Node<EntryNode> last;
 
-    public PriorityQueueLinkSort() {
-        this.first = null;
+    public PriorityQueueLinkSort() { // No hay parametros para inicializar la cola vacía
+        this.first = null; // first y last son null para representar una cola vacía
         this.last = null;
     }
 
     @Override
     public void enqueue(E x, N pr) {
+        EntryNode entradaNueva = new EntryNode(x, pr);
+        Node<EntryNode> nodoNuevo = new Node<>(entradaNueva);
+
+        // 1° Caso: Si la cola está vacía
+        if (first == null) {
+            first = last = nodoNuevo;
+        }
+
+        // 2° Caso: Si se inserta al inicio
+        else if (pr.compareTo(first.data.priority) < 0) {
+            nodoNuevo.next = first;
+            first = nodoNuevo;
+        }
+
+        // 3° Caso: Si se inserta al medio o al final
+        else {
+            Node<EntryNode> nodoCurrent = first;
+            Node<EntryNode> nodoPrevio = null;
+
+            while (nodoCurrent != null && pr.compareTo(nodoCurrent.data.priority) >= 0) {
+                nodoPrevio = nodoCurrent;
+                nodoCurrent = nodoCurrent.next;
+            }
+
+            nodoNuevo.next = nodoCurrent;
+            nodoPrevio.next = nodoNuevo;
+
+            if (nodoCurrent == null) {
+                last = nodoNuevo;
+            }
+        }
     }
 
     @Override

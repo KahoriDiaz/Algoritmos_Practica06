a73f240b8c372d5575917e569015f98b3c498c11
Rosario Cahuana Pari <rosariocahuanapari@gmail.com>
2025-05-03 16:44:28 -0500

    Implementacion del metodo enqueue en QueueArray en ejercicio2

--- ejercicio2/QueueArray.java
+++ ejercicio2/QueueArray.java
@@ -5,22 +5,27 @@ import actividad2.Queue;
 
 public class QueueArray<E> implements Queue<E> {
     private E[] data;
-    private int inicio;
-    private int fin;
+    private int first;
+    private int last;
     private int cantidad;
 
     public QueueArray(int tam) {
         data = (E[]) new Object[tam];
-        inicio = 0;
-        fin = 0;
+        first = 0;
+        last = 0;
         cantidad = 0;
     }
 
+    // Inserta un elemento al final de la cola
     @Override
     public void enqueue(E x) {
-
+        if(cantidad == data.length){
+            throw new IllegalStateException("La cola esta llena. No se puede insertar.");
+        }
+        data[last] = x;
+        last = (last + 1) % data.length;
+        cantidad++;
     }
-
     @Override
     public E dequeue() throws ExceptionIsEmpty {
         return null;

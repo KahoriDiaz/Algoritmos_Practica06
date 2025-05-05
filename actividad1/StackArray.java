27d60b65d70add6823ac57662d6ab110cfd2d8ee
Janitza Tapia <janitzat4@gmail.com>
2025-05-04 22:30:55 -0500

    Comentarios Actividad1

--- actividad1/StackArray.java
+++ actividad1/StackArray.java
@@ -8,59 +8,58 @@ public class StackArray<E> implements Stack<E> {
         this.array = (E[]) new Object[n];
         this.tope = -1;
     }
-
+    //agregar un elemento a la pila
     public void push(E x) {
+        //aso en el que ya la pila esta llena
         if (tope < array.length - 1) { // -> si hay espacio en la pila
-            array[++tope] = x;  // -> incrementa tope y coloca el nuevo elemento
+            array[++tope] = x;  // -> 1ro incrementa tope y luego asigna el valor al indice incrementado
         } else {
             System.out.println("Pila llena");
         }
     }
-
+    //eliminar y devolver el ULTIMO elemento agregado a la pila
     public E pop() throws ExceptionIsEmpty {
         if (isEmpty()) {  // -> si la pila está vacía
             throw new ExceptionIsEmpty("Array vacío");
         }
-        return array[tope--];  // -> devuelve y elimina el elemento del tope
+        return array[tope--];  // -> 1ro devuelve el elemento y luego decrementa
     }
-
+    //devuelve el elemento en la cima (tope) de la pila sin eliminarlo
     public E top() throws ExceptionIsEmpty {
         if (isEmpty()) {  // -> si la pila está vacía
             throw new ExceptionIsEmpty("Array vacío");
         }
         return array[tope];    // -> devuelve el elemento del tope sin eliminarlo
     }
-
+    //verifica si la pila está vacía
     public boolean isEmpty() {
         return tope == -1;  // -> retorna true si no hay elementos
     }
-
+    //vacía la pila
     public void destroyStack() {
         tope = -1;  // -> reinicia la pila dejándola vacía (tope=-1 -> vacía)
     }
-
+    //si la pila esta llena
     public boolean isFull() {
         return tope == array.length - 1; // -> retorna true si ya no hay espacio para más elementos
     }
-
+    //representar visualmente el contenido de la pila en forma de una cadena
     public String toString() {
         if (isEmpty()) {
             return "Pila vacía";              // -> si no hay elementos
         }
-
+        //comenzamos a construir el texto
         String resultado = "Pila: [";
-
+        //Recorremos desde la base hasta el tope
         for (int i = 0; i <= tope; i++) {
-            resultado += array[i];            // -> agrega cada elemento
+            resultado += array[i];            // ->  Agregamos el elemento actual
             if (i < tope) {
-                resultado += ", ";            // -> separador entre elementos
+                resultado += ", ";            // -> Agregamos coma si no es el último
             }
         }
 
         resultado += "]";  // -> cierra la cadena con corchete
         return resultado;  // -> retorna la cadena final
     }
-    
-
     
 }
